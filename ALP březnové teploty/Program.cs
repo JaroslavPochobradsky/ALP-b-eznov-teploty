838b6a6ce771ea53e8c6c9de7f99009a7fd61a04
Jaroslav Pochobradský <jaroslav.pochobradsk@educhem.cz>
2023-03-10 11:36:15 +0100

    repaired average

--- ALP březnové teploty/Program.cs
+++ ALP březnové teploty/Program.cs
@@ -1,43 +1,43 @@
-﻿// zde zadáme hodnoty teplot
+﻿// Zde zadáme hodnoty teplot.
 int[] poleTeplot = new int[31];
 Console.WriteLine("Zadejte hodnoty: ");
 for (int i = 0; i < 31; i++)
 {
     poleTeplot[i] = int.Parse(Console.ReadLine());
 }
-// máme zadané hodnoty teplot a zadáme co chceme vypsat
+// Máme zadané hodnoty teplot a zadáme co chceme vypsat.
+
 Console.WriteLine("Jakou teplotu mám vypsat?");
 Console.WriteLine("minimum, maximum, průměr");
 string hodnota = Console.ReadLine();
-// zde máme zadáno co chceme vypsat a program začne vypisovat
+// Zde máme zadáno co chceme vypsat a program začne vypisovat.
+
 if (hodnota == "minimum")
 {
     int min = poleTeplot[0];
     foreach (int i in poleTeplot)
     {
-        min = Math.Min(min, poleTeplot[i + 1]);
+        min = Math.Min(min, poleTeplot[i]);
     }
-    Console.WriteLine(min);
-    // tady program vypíše minimální hodnotu
+    Console.WriteLine(min); // Tady program vypíše minimální hodnotu.
 }
+
 else if (hodnota == "maximum")
 {
     int max = poleTeplot[0];
     foreach (int i in poleTeplot)
     {
-        max = Math.Max(max, poleTeplot[i + 1]);
+        max = Math.Max(max, poleTeplot[i]);
     }
-    Console.WriteLine(max);
-    // tady maximální hodnotu
+    Console.WriteLine(max); // Tady maximální hodnotu.
 }
+
 else
 {
-    int cisla = poleTeplot[0];
-    for (int num = 1; num < 31; num++)
+    double prumer = poleTeplot[0];
+    foreach (int i in poleTeplot)
     {
-        cisla = cisla + poleTeplot[num];
+        prumer = poleTeplot.Average();
     }
-    double prumer = cisla / 31;
-    Console.WriteLine(prumer);
-    // a tady průměr
+    Console.WriteLine("Průměr teplot je: " + prumer);
 }
